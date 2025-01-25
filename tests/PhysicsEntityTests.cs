485e07668aa161a508dd40c13057805b268857a8
Phill <philldamaskin@gmail.com>
2025-01-25 15:59:29 -0500

    add remote testing and package info

---
+++ tests/PhysicsEntityTests.cs
@@ -0,0 +1,16 @@
+﻿using Physics.Components;
+using Worlds;
+
+namespace Physics.Tests
+{
+    public class PhysicsEntityTests : PhysicsTests
+    {
+        [Test]
+        public void VerifyBodyIsItself()
+        {
+            using World world = CreateWorld();
+            Body body = new(world, Shape.Create(new SphereShape(0.5f)), IsBody.Type.Dynamic);
+            Assert.That(body.Is(), Is.True);
+        }
+    }
+}
---
+++ tests/PhysicsTests.cs
@@ -0,0 +1,23 @@
+﻿using Types;
+using Worlds;
+using Worlds.Tests;
+
+namespace Physics.Tests
+{
+    public abstract class PhysicsTests : WorldTests
+    {
+        static PhysicsTests()
+        {
+            TypeRegistry.Load<Physics.TypeBank>();
+            TypeRegistry.Load<Transforms.TypeBank>();
+        }
+
+        protected override Schema CreateSchema()
+        {
+            Schema schema = base.CreateSchema();
+            schema.Load<Physics.SchemaBank>();
+            schema.Load<Transforms.SchemaBank>();
+            return schema;
+        }
+    }
+}
