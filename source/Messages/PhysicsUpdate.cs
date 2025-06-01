932880089ba4a724e4e529dd1990c37a5663c527
Phill <philldamaskin@gmail.com>
2025-06-01 12:35:30 -0400

    add update message

---
+++ source/Messages/PhysicsUpdate.cs
@@ -0,0 +1,12 @@
+﻿namespace Physics.Messages
+{
+    public readonly struct PhysicsUpdate
+    {
+        public readonly double deltaTime;
+
+        public PhysicsUpdate(double deltaTime)
+        {
+            this.deltaTime = deltaTime;
+        }
+    }
+}
